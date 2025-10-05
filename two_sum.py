403c8725fdc02b30ae71bcf1ce664d68bf8ee744
david <dmsim159@gmail.com>
2025-10-05 13:29:16 -0700

    Return i + j in a list if = to target

--- two_sum.py
+++ two_sum.py
@@ -41,3 +41,5 @@ class Solution:
         n = len(nums)
         for i in range(n):
             for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
