029fd220365399a1c443bd3e1e0b5d46879830bf
david <dmsim159@gmail.com>
2025-10-05 14:12:44 -0700

    Find two indices whose values sum to target

--- two_sum.py
+++ two_sum.py
@@ -51,5 +51,10 @@ class Solution:
         hashmap = {}
         for i, num in enumerate(nums):
             hashmap[num] = i
+            
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in hashmap and hashmap[diff] != i:
+                return [i, hashmap[diff]]
         
         
