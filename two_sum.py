566c1d1bd2b4b8ad874f9563abd6e6ef3de9cbf9
david <dmsim159@gmail.com>
2025-10-05 14:04:58 -0700

    Initial boilerplate code

--- two_sum.py
+++ two_sum.py
@@ -43,3 +43,9 @@ class Solution:
             for j in range(i + 1, n):
                 if nums[i] + nums[j] == target:
                     return [i, j]
+                
+# optimal solution
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
