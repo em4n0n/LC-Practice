47e70cbfc43827d2c427eb4dafbf8b31f7b601ac
David Sim <58372945+em4n0n@users.noreply.github.com>
2025-10-11 16:10:54 -0700

    Question

--- missing_number.py
+++ missing_number.py
@@ -1,42 +1,54 @@
 """
-Given an array `nums` containing `n` distinct numbers in the range `[0, n]`, return *the only number in the range that is missing from the array.*
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
 
-**Example 1:**
+ 
 
-**Input:** nums = [3,0,1]
+Example 1:
 
-**Output:** 2
+Input: nums = [3,0,1]
 
-**Explanation:**
+Output: 2
 
-`n = 3` since there are 3 numbers, so all numbers are in the range `[0,3]`. 2 is the missing number in the range since it does not appear in `nums`.
+Explanation:
 
-**Example 2:**
+n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
 
-**Input:** nums = [0,1]
+Example 2:
 
-**Output:** 2
+Input: nums = [0,1]
 
-**Explanation:**
+Output: 2
 
-`n = 2` since there are 2 numbers, so all numbers are in the range `[0,2]`. 2 is the missing number in the range since it does not appear in `nums`.
+Explanation:
 
-**Example 3:**
+n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
 
-**Input:** nums = [9,6,4,2,3,5,7,0,1]
+Example 3:
 
-**Output:** 8
+Input: nums = [9,6,4,2,3,5,7,0,1]
 
-**Explanation:**
+Output: 8
 
-`n = 9` since there are 9 numbers, so all numbers are in the range `[0,9]`. 8 is the missing number in the range since it does not appear in `nums`.
+Explanation:
 
-**Constraints:**
+n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
 
-- `n == nums.length`
-- `1 <= n <= 104`
-- `0 <= nums[i] <= n`
-- All the numbers of `nums` are **unique**.
+ 
+ 
+
+ 
+
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 104
+0 <= nums[i] <= n
+All the numbers of nums are unique.
+ 
+
+Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
 """
 def missing_number(nums):
     return sum(range(len(nums)+1)) - sum(nums)
